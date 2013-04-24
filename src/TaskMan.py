352ad7956b79d5b2c40cc530248560db952c6834
Ambidextrous <ravisinha@my.unt.edu>
2013-04-24 01:37:05 -0500

    Add str, repr, and is_overdue

--- src/TaskMan.py
+++ src/TaskMan.py
@@ -4,6 +4,7 @@ Reads a record with comma separated fields
 Agnostic to whether the record was a string or a csv
 """
 
+from datetime import datetime
 
 class TaskMan(object):
     def __init__(self, string):
@@ -15,3 +16,15 @@ class TaskMan(object):
         self.month = items[3]
         self.day = items[4]
 
+    def __str__(self):
+        return '{0}, task {1}, due {2}-{3}-{4}'.format(self.user, self.task_info, self.month, self.day, self.year) 
+        
+    def __repr__(self):
+        return '{0}, task {1}, due {2}-{3}-{4}'.format(self.user, self.task_info, self.month, self.day, self.year) 
+
+    def is_overdue(self):
+        """ Return if the task in question is overdue """
+        date = datetime.now()
+        due_date = datetime(int(self.year), int(self.month), int(self.day), 23, 59, 59)
+        return date > due_date
+        
--- tests/TestTaskMan.py
+++ tests/TestTaskMan.py
@@ -14,6 +14,29 @@ class TestTaskMan(unittest.TestCase):
         self.assertEqual(task_details_obtained.month, '4')
         self.assertEqual(task_details_obtained.day, '25')
 
+    def test_str_repr(self):
+        """ Test the string representation of the task """
+        task = 'Ravi Sinha,Do Laundry,2013,4,25'
+        task_details_obtained = TaskMan(task)
+        self.assertEqual(str(task_details_obtained), 'Ravi Sinha, task Do Laundry, due 4-25-2013')
+
+    def test_repr_repr(self):
+        """ Test the string representation of the task """
+        task = 'Ravi Sinha,Do Laundry,2013,4,25'
+        task_details_obtained = TaskMan(task)
+        self.assertEqual(repr(task_details_obtained), 'Ravi Sinha, task Do Laundry, due 4-25-2013')
+
+    def test_overdue(self):
+        """ Test the overdue test """
+        task = 'Ravi Sinha,Do Laundry,2013,4,19'
+        task_details_obtained = TaskMan(task)
+        self.assertTrue(task_details_obtained.is_overdue()) 
+
+    def test_overdue_negative(self):
+        """ Test when not overdue """
+        task = 'Ravi Sinha,Fly to Moon,9999,4,25'
+        task_details_obtained = TaskMan(task)
+        self.assertFalse(task_details_obtained.is_overdue())
+
 if __name__ == '__main__':
     unittest.main()
-
