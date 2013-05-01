1ae0abc07956a0836ac44423711d4c8de0fb5465
Ambidextrous <ravisinha@my.unt.edu>
2013-04-30 20:24:28 -0500

    Test adding tasks to the task collection

--- tests/TestTaskCollection.py
+++ tests/TestTaskCollection.py
@@ -14,13 +14,19 @@ from Task import Task
 class TestTaskCollection(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        user = 'Ravi Sinha'
         global tasks
+        global task_details_obtained
+        user = 'Ravi Sinha'
+        task = 'Do Laundry,2013,4,25'
         tasks = TasksCollection(user)
+        task_details_obtained = Task(task)
 
     def test_constuctor(self):
         self.assertEqual(tasks.user, 'Ravi Sinha')
 
+    def test_adding_task(self):
+        tasks.add(task_details_obtained)
+        self.assertEqual(tasks.tasks[0].task_info, 'Do Laundry')
     
 if __name__ == '__main__':
     unittest.main()
