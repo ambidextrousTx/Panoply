faaef1c01ab10a8e3f10f482e7ca2f0c4e5bf405
Ambidextrous <ravisinha@my.unt.edu>
2013-05-12 16:40:48 -0500

    Start Panoply class to handle messages from REPL

--- src/panoply.py
+++ src/panoply.py
@@ -3,8 +3,9 @@ The main module for Panoply
 
 Puts the user in a kind of a REPL
 Allows the user to create TasksCollection's and Task's
+The shell also corresponds to a Panoply object that handles requests
 
-No unit tests for this module
+No unit tests for this module, yet
 
 Ambidextrous
 May 3, 2013
@@ -14,6 +15,25 @@ from __future__ import print_function
 import sys
 
 PANOPLY_VERSION = '0.1'
+
+def Panoply(object):
+    def __init__(self):
+        self.task_collection_name = ''
+
+    def start(self, name):
+        self.task_collection_name = name
+
+    def load(self, name):
+        """ Load contents of a previously saved file """
+        pass
+
+    def add(self, task):
+        """ Add one task to the collection """
+        pass
+
+    def scan(self):
+        """ Scan the collection for any overdue tasks """
+        pass
 
 def print_greeting():
     print('Welcome to Panoply version {0}'.format(PANOPLY_VERSION), end = '\n')
@@ -37,7 +57,6 @@ def process_request(request):
     else:
         command = get_command(request)
         
-
 def run_repl():
     request = 'X'
     while request != '':
