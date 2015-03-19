a2776a754bf3d879d90fa4c3cb267f204a98ebe4
ambidextrousTx <ravisinha@my.unt.edu>
2015-03-18 23:05:42 -0500

    Clean up tests for the main module

--- tests/TestPanoply.py
+++ tests/TestPanoply.py
@@ -6,13 +6,26 @@ import panoply
 
 
 class PanoplyTest(unittest.TestCase):
+    ''' Tests for the main Panoply class
+    '''
     def setUp(self):
+        ''' Nothing for now
+        '''
         pass
 
-    def test_sanity(self):
+    def test_sanity_false(self):
+        ''' Not everything is allowed to go through the
+        Panoply interface '''
         self.assertFalse(panoply.sanity_check('random string'))
 
+    def test_sanity_true(self):
+        ''' There is a set of expected commands for the
+        Panoply interface '''
+        self.assertTrue(panoply.sanity_check('display'))
+
     def test_get_command(self):
+        ''' Currently the first word of a command line command
+        is the one that gets passed on '''
         self.assertEqual(panoply.get_command('this is random'), 'this')
 
     def tearDown(self):
